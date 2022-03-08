27440f07294e9138d368a23244446f82f56f8b85
Tony Pruvchev <kevingoldmaier27@gmail.com>
2022-03-08 22:34:07 +0200

    first-commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const myName = require('./branch1');
+
+const sayHello = (myName) => {
+    console.log(`Welcome back master ${myName}!`);
+}
+
+sayHello(myName);
---
+++ branch1.js
@@ -0,0 +1,3 @@
+const name = 'Tony';
+
+module.exports = name;
